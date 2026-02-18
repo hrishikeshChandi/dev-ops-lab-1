789be9781767ab5f0dc2fee1d7a707624c3b25b4
Hrishikesh Chandi <143275248+hrishikeshChandi@users.noreply.github.com>
2026-02-18 10:21:10 +0530

    Add check for WEATHER_API_KEY in tests

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-console.log("All tests passed!");
+if (!process.env.WEATHER_API_KEY) {
+  console.error("Test Failed: WEATHER_API_KEY is missing!");
+  process.exit(1);
+}
+
+console.log("All tests passed! WEATHER_API_KEY is set.");
